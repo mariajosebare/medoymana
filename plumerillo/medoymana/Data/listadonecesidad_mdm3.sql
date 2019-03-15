487efb19168cc4bff03ac9aece75da02f605d9d0
mariajosebare <mara2013.mjb@gmail.com>
2019-03-15 06:07:11 -0300

    Arreglos en necesidades

--- plumerillo/medoymana/Data/listadonecesidad_mdm3.sql
+++ plumerillo/medoymana/Data/listadonecesidad_mdm3.sql
@@ -3,30 +3,30 @@ USE mdm3;
 
 INSERT INTO necesidad (ID_habilidad,ID_usuario,descripcion_necesidad,fecha_creado) VALUES
 
-(50,1,"Hola necesito quien pueda sacar fotos en un cumpleaño",07/03/2019),
-(37,1,"Hola necesito quien pueda filmar en un cumpleaño a realizarce el dia 7 de abril",07/03/2019),
-(50,1,"Estaria necesitando para el dia 7 de abril quien realice tortas de cumpleaño",07/03/2019),
-(2,1,"Necesito persona idonea en la parrilla realizar azado y verduras a la parrilla para festejo de cumpleaño",07/03/2019),
+(25,1,"Hola necesito quien pueda sacar fotos en un cumpleaño",07/03/2019),
+(26,1,"Hola necesito quien pueda filmar en un cumpleaño a realizarce el dia 7 de abril",07/03/2019),
+(13,1,"Estaria necesitando para el dia 7 de abril quien realice tortas de cumpleaño",07/03/2019),
+(45,1,"Necesito persona idonea en la parrilla realizar azado y verduras a la parrilla para festejo de cumpleaño",07/03/2019),
 
 (19,2,"Necesito electricista para cambiar enchufe que hizo corto, realizo decoracion",07/03/2019),
-(32,2,"Necesito albañil para reparación de muro vereda realizo limpiezas",07/03/2019),
-(32,2,"Necesito realizar filmación de cumpleaño infantil",07/03/2019),
-(13,2,"Necesito quien realice manualidades para la realizacion de souvenirs para fiesta infantil",07/03/2019),
+(1,2,"Necesito albañil para reparación de muro vereda realizo limpiezas",07/03/2019),
+(26,2,"Necesito realizar filmación de cumpleaño infantil",07/03/2019),
+(37,2,"Necesito quien realice manualidades para la realizacion de souvenirs para fiesta infantil",07/03/2019),
 
-(25,3,"Hola estaria necesitando quien pueda reparar una pc ya que fui a prenderla y no inicia",07/03/2019),
-(26,3,"Necesito realizar limpieza de salon de fiesta (despues de cumpleaño infantil)",07/03/2019),
-(16,3,"Necesito electricista para reparación y cambio de cajas termicas puedo trabajar como niñera",07/03/2019),
-(25,3,"Necesito alguien que se desempeñe idoneamente en la cocina",07/03/2019),
+(50,3,"Hola estaria necesitando quien pueda reparar una pc ya que fui a prenderla y no inicia",07/03/2019),
+(32,3,"Necesito realizar limpieza de salon de fiesta (despues de cumpleaño infantil)",07/03/2019),
+(22,3,"Necesito electricista para reparación y cambio de cajas termicas puedo trabajar como niñera",07/03/2019),
+(13,3,"Necesito alguien que se desempeñe idoneamente en la cocina",07/03/2019),
 
-(13,4,"Necesito albañil para reparacion de techo puedo realizar tareas de cocina",07/03/2019),
-(13,4,"Necesito reparador de PC para formateo y service de laptop",07/03/2019),
-(16,4,"Necesito elcticista para la instalacion de un nuevo enchufe,puedo desempeñarme como niñera",07/03/2019),
-(32,4,"Necesito realizar la filmación de evento",07/03/2019),
+(1,4,"Necesito albañil para reparacion de techo puedo realizar tareas de cocina",07/03/2019),
+(50,4,"Necesito reparador de PC para formateo y service de laptop",07/03/2019),
+(22,4,"Necesito elcticista para la instalacion de un nuevo enchufe,puedo desempeñarme como niñera",07/03/2019),
+(26,4,"Necesito realizar la filmación de evento",07/03/2019),
 
-(22,5,"Necesito decorar salon de fiestas para cumpleaño infantil, me desempeño como electicista",07/03/2019),
-(1,5,"Necesito persona que limpie,realizo trabajos de albañileria",07/03/2019),
-(45,5,"Necesito quien pueda realizar armado de redes",07/03/2019),
-(22,5,"Necesito niñera para cuidado de 2 niñas,realizo trabajo de electricidad",07/03/2019);
+(19,5,"Necesito decorar salon de fiestas para cumpleaño infantil, me desempeño como electicista",07/03/2019),
+(32,5,"Necesito persona que limpie,realizo trabajos de albañileria",07/03/2019),
+(2,5,"Necesito quien pueda realizar armado de redes",07/03/2019),
+(16,5,"Necesito niñera para cuidado de 2 niñas,realizo trabajo de electricidad",07/03/2019);
 
 
 
