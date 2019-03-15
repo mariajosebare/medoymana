3fffd4ed3a58b504d4eb7731c8a96dbf861d6069
Carolina Gherzo <carolinagherzo@gmail.com>
2019-03-15 02:30:58 -0300

    Agregado de CSS propio

--- plumerillo/medoymana/persistencia/BaseDeDatos.py
+++ plumerillo/medoymana/persistencia/BaseDeDatos.py
@@ -4,7 +4,7 @@ import pymysql
 __cursor = pymysql.connect(
     host = "127.0.0.1",
     user="root",
-    password="root",
+    password="asd123",
     db="mdm3",
     cursorclass=pymysql.cursors.DictCursor
 ).cursor()
