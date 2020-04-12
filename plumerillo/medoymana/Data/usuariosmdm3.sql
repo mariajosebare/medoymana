bef417a8ef76ec460225f1e04b3aec6b94856c8a
AnaCasalas <belcasalas@gmail.com>
2020-04-12 00:22:42 -0300

    Base de datos, comilla

--- plumerillo/medoymana/Data/usuariosmdm3.sql
+++ plumerillo/medoymana/Data/usuariosmdm3.sql
@@ -2,6 +2,6 @@ USE mdm3;
 INSERT INTO usuario (ci,nombre,apellido,fecha_nacimiento,email,telefono,calle,numero_puerta,esquina_1,esquina_2) VALUES
 (42481555,"María Carolina","Gherzo Ferreira",'1989/03/21',"carolinagherzo@gmail.com",091400188,"Emilio Raña",2968,"Pedro Vidal","José Catala"),
 (46488171,"Cecilia","Varela",'1994/03/12',"cjvs-94@hotmail.com",094025206,"Manuel Meléndez",3980,"Santiago Sierra","Rafael Hortiguera"),
-(50884965,"Ana Belen","Casalas",'1994/10/21'',"belcasalas@gmail.com",099063738," 4 de Julio",3401,"Luis Alberto de Herrera","Julio Cesar"),
+(50884965,"Ana Belen","Casalas",'1994/10/21',"belcasalas@gmail.com",099063738," 4 de Julio",3401,"Luis Alberto de Herrera","Julio Cesar"),
 (42826561,"María José","Bareño",'1976/12/10',"mara2013.mjb@gmail.com",098214589,"Dr José María Penco",3295,"Dr José Pugnalini","Dr Fermin Ferreira"),
 (25963758,"Pablo Martín","Tort",'1978/08/01',"pablotort@hotmail.com",098774722,"Avenida Millán","5269a","Avenida Luis Alberto de Herrera","Reyes");
