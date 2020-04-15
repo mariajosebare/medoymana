49b251aa12b88ab573046528266587e5cdbacf21
AnaCasalas <belcasalas@gmail.com>
2020-04-15 20:01:10 -0300

    Chat

---
+++ plumerillo/medoymana/Data/chat.sql
@@ -0,0 +1,30 @@
+USE mdm3;
+/* Procedemos a ingresar los mensajes del chat*/
+
+INSERT INTO chat (ID_usuario_1,ID_usuario_2,mensaje,fecha_hora_mensaje) VALUES
+
+(1,3,"Hola, ¿cómo estás? puedes realizar un book de fotos del cumpleaños?",'2020-04-15-10:00:00'),
+(3,1,"Hola, todo bien. Cuándo es el cumple?",'2020-04-15-10:02:00'),
+(1,3,"Es el sábado, en la noche",'2020-04-15-10:05:00'),
+(3,1,"Perfecto, a cambio podrías reparar mi pc? ",'2020-04-15-10:07:00'),
+(1,3,"Si, claro",'2020-04-15-10:09:00'),
+(1,3,"Bien",'2020-04-15-10:10:00'),
+
+(3,5,"Hola, ¿cómo estás? necesito que reparen un enchufe que hizo corto, a cambio puedo ser baby sitter por 4 o 5 horas",'2020-04-14-14:00:00'),
+(5,3,"Hola, perfecto, son dos niñas, podrías cuidarlas el domingo de 14 a 19?",'2020-04-14-14:02:00'),
+(3,5,"Puedo, por qué zona estás? Para el arreglo que materiales necesito comprar? ",'2020-04-14-14:06:00'),
+(5,3,"Solo el enchufe que qieras poner, el resto de los materiales puedo llevar",'2020-04-14-14:08:00'),
+(3,5,"Buenisimo! Qué día podes venir?",'2020-04-14-14:10:00'),
+(5,3,"Mañana después del mediodía te queda bien?",'2020-04-14-14:11:00'),
+(3,5,"Sí perfecto",'2020-04-14-14:13:00');
+
+
+
+
+
+
+
+
+
+
+
---
+++ plumerillo/medoymana/persistencia/Chat.py
@@ -0,0 +1,10 @@
+from plumerillo.medoymana.persistencia import BaseDeDatos, Usuarios
+
+def obtener_mensajes(id1,id2):
+    mensajes = BaseDeDatos.correr_sql(f"SELECT * FROM chat WHERE ID_usuario_1 = {id1} and ID_usuario_2 = {id2}")
+    return mensajes
+
+
+def agregar_mensajes(id1,id2, mensaje):
+    mensajes=BaseDeDatos.correr_sql(f"INSERT INTO chat (ID_usuario_1,ID_usuario_2,mensaje,fecha_hora_mensaje) VALUES ({id1},{id2},'{mensaje}',CURRENT_TIMESTAMP())")
+    return True
